f5389646b9c02535d30d34add5c53f2bc218bb86
DeniseBarbosa <deniseluvoir@gmail.com>
2023-01-15 10:24:58 -0300

    Add project files.

---
+++ AgendaDeContatos/Repositorio/IContatoRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using AgendaDeContatos.Models;
+using System.Collections.Generic;
+
+namespace AgendaDeContatos.Repositorio
+{
+    public interface IContatoRepositorio
+    {
+        List<ContatoModel> BuscarTodos();
+        ContatoModel Adicionar(ContatoModel contato);
+    }
+}
