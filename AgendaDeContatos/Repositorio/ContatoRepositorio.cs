9fe3c029324d31f724d8a4fbb574a0934f57a48c
DeniseBarbosa <deniseluvoir@gmail.com>
2023-01-15 11:55:12 -0300

    Alterar o registro

--- AgendaDeContatos/Repositorio/ContatoRepositorio.cs
+++ AgendaDeContatos/Repositorio/ContatoRepositorio.cs
@@ -12,7 +12,10 @@ namespace AgendaDeContatos.Repositorio
         {
             _bancoContext = bancoContext;
         }
-
+        public ContatoModel ListarPorId(int id)
+        {
+            return _bancoContext.Contatos.FirstOrDefault(x =>x.Id== id);
+        }
         public List<ContatoModel> BuscarTodos()
         {
             return _bancoContext.Contatos.ToList();
@@ -25,6 +28,18 @@ namespace AgendaDeContatos.Repositorio
             return contato;
         }
 
-       
+        public ContatoModel Atualizar(ContatoModel contato)
+        {
+            ContatoModel contatoDB = ListarPorId(contato.Id);
+            if (contatoDB == null) throw new System.Exception("Houve um erro na atualização do contato");
+            contatoDB.Nome = contato.Nome;
+            contatoDB.Email = contato.Email;
+            contatoDB.Celular = contato.Celular;
+
+            _bancoContext.Contatos.Update(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return contatoDB;
+        }
     }
 }
--- AgendaDeContatos/Repositorio/IContatoRepositorio.cs
+++ AgendaDeContatos/Repositorio/IContatoRepositorio.cs
@@ -5,7 +5,9 @@ namespace AgendaDeContatos.Repositorio
 {
     public interface IContatoRepositorio
     {
+        ContatoModel ListarPorId(int id);
         List<ContatoModel> BuscarTodos();
         ContatoModel Adicionar(ContatoModel contato);
+        ContatoModel Atualizar(ContatoModel contato);
     }
 }
